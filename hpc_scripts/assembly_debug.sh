4689e36e93170c29263e25562da623e7b35cb748
harrychown <harrychown98@gmail.com>
2021-11-25 10:08:50 +0000

    Fix: contig conditional for floats, Add: comment code for gene-calling only

--- hpc_scripts/assembly_debug.sh
+++ hpc_scripts/assembly_debug.sh
@@ -1,12 +1,12 @@
-conda activate bbtools
+PREFIX=C30
 stats.sh ./mask_$PREFIX/$PREFIX.contigs.fa.masked format=2 > ./$PREFIX.stats
 STAT=$(grep '% main' ./$PREFIX.stats | cut -d$'\t' -f2 | sed 's/%//')
 MINCONTIG=0
-if [ "$STAT" lt 40 ] 
+CUTOFF=40
+if [ 1 -eq "$(echo "${STAT} < ${CUTOFF}" | bc)" ]
 then
 	MINCONTIG=5000
 else
 	MINCONTIG=1000
 fi
-echo $STAT
-echo $MINCONTIG
+
