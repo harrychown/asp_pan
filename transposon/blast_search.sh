8fa34225d120aaacfb24adb0ad97585adb83d778
harrychown <harrychown98@gmail.com>
2022-01-14 11:35:32 +0000

    Update: preparation for ITR-ORF-ITR search, change BLAST output

--- transposon/blast_search.sh
+++ transposon/blast_search.sh
@@ -1,23 +1,22 @@
 source activate blastenv
 cd /data/pan_transposon
-#makeblastdb -in centroids.fasta -dbtype prot -out pan_blast_db
-# Search the protein of ORF against pan-genome
-#blastp -db pan_blast_db -query protein.fasta -out protein_vs_pangenome -outfmt "6 qseqid sseqid pident qcovhsp length qstart qend sstart send evalue" 
 
-
-#makeblastdb -in ARAF001.pep.fa -dbtype prot -out araf_pep_db
-# Search the protein of ORF against ARAF001 proteome
-#blastp -db araf_pep_db -query protein.fasta -out protein_vs_proteome -outfmt "6 qseqid sseqid pident qcovhsp length qstart qend sstart send evalue" 
-
-makeblastdb -in ARAF001.contigs.fa -dbtype nucl -out araf_db
-# Search the ORF gainst ARAF001 genome
-blastn -db araf_db -query orf.fasta -out orf_vs_araf -outfmt "6 qseqid sseqid frames pident qcovhsp length qstart qend sstart send evalue"  
-# Search the locus against ARAF001 genome
-#blastn -db araf_db -query locus.fasta -out locus_vs_araf -outfmt "6 qseqid sseqid pident qcovhsp length qstart qend sstart send evalue" 
+GENOME=ARAF001.contigs.fa
+: <<'END'
+# 1) PERFORM ALIGNMENTS
+makeblastdb -in $GENOME -dbtype nucl -out genome_db
+# Search the ORF gainst genome
+blastn -db genome_db -query orf.fasta -out orf_vs_genome -outfmt "6 qseqid sseqid frames pident qcovhsp length qstart qend sstart send evalue"  
+# Search TIR against genome
+blastn -db genome_db -query tir.fasta -out tir_vs_genome -outfmt "6 qseqid sseqid frames pident qcovhsp length qstart qend sstart send evalue" 
+END
 
 
 
-# Search TIR against ARAF001 genome
-blastn -db araf_db -query tir.fasta -out tir_vs_araf -outfmt "6 qseqid sseqid frames pident qcovhsp length qstart qend sstart send evalue" 
+
+# We could use bedtools closest to identify if they are flanking
+# To do this, we need to first make a BED file out of the BLAST results
+cat balst_tabular_output.txt | awk '{print($2"\t"$7-1"\t"$8)}' > blast_otput.bed
+
 
 
