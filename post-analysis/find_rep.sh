e39f759d4cf519f1ebc8c34e4689499bb64a5ad2
harrychown <harrychown98@gmail.com>
2022-04-05 10:13:12 +0100

    Update: Find repeated genes in pan-genome

--- post-analysis/find_rep.sh
+++ post-analysis/find_rep.sh
@@ -1,3 +1,5 @@
 # Analyse repeated genes
 awk -F "\"" '{ print $4 }' mock_pangenome.gtf | sort | uniq -c | sort -k1 -h | sed 's/^[[:space:]]*//g' | head -n -1 | sed 's/ /,/g' > frequency.csv
 grep -v "1," frequency.csv > multifreq.csv
+# Identify the frequency of frequencies
+awk -F "," '{ print $1 }' frequency.csv | uniq -c | sed 's/^[[:space:]]*//g' | sed 's/ /,/g' > frequency_frequencies.csv
